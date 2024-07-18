d8fd89682391509a6a1ae07ad37659b9b6ec07b3
Łukasz Mastalerz <lukasz.mastalerz@buddy.works>
2024-07-18 13:19:34 +0200

    Finding alternative path algorithm

--- TrainStationsRepo.API/Post.cs
+++ TrainStationsRepo.API/Post.cs
@@ -3,6 +3,7 @@
 public record Post(int Id, string Name, string Type)
 {
     private List<GatewayDistances> _distances = [];
+    public IEnumerable<Post> Directions => _distances.Select(x => x.Direction);
     public virtual bool Equals(Post? other) => other?.Id == Id;
     public override int GetHashCode() => Id.GetHashCode();
 
@@ -10,6 +11,21 @@ public record Post(int Id, string Name, string Type)
     {
         _distances = distances;
     }
+    
+    public Post? FastestDirectionTo(Post end, List<Post> ignoredPosts)
+    {
+        (Post direction, int value)? minimum = null;
+        foreach (var (direction, costs) in _distances)
+        {
+            if(ignoredPosts.Contains(direction)) continue;
+            var cost = costs[end];
+            if(cost == int.MaxValue) continue;
+            if (direction == end) return end;
+            if (minimum is not null && minimum.Value.value <= cost) continue;
+            minimum = (direction, cost);
+        }
+        return minimum?.direction;
+    }
 }
 
-public record GatewayDistances(int Direction, Dictionary<Post, int> Costs);
+public record GatewayDistances(Post Direction, Dictionary<Post, int> Costs);
--- TrainStationsRepo.API/World.cs
+++ TrainStationsRepo.API/World.cs
@@ -43,7 +43,7 @@ class World
                     indexedValue => posts[indexedValue.i],
                     indexedValue => (int)indexedValue.value
                     );
-            var gatewayDistances = new GatewayDistances(set[1], distances);
+            var gatewayDistances = new GatewayDistances(posts[set[1]], distances);
             if(distancesList.ContainsKey(set[0])) distancesList[set[0]].Add(gatewayDistances);
             else distancesList.Add(set[0], [gatewayDistances]);
         }
@@ -56,11 +56,66 @@ class World
         return world;
     }
 
+    private IEnumerable<Post> FastestPath(Post start, Post end, List<Post>? ignoredPosts = null, bool softLoop = true)
+    {
+        List<Post> steps = [start];
+        List<Post> ignore = ignoredPosts?.ToList() ?? [];
+        ignore.Add(start);
+        var next = start.FastestDirectionTo(end, ignore);
+        if (next is null) return [];
+        
+        ignore.Add(next);
+        steps.Add(next);
+        while (next != end)
+        {
+            var step = next.FastestDirectionTo(end, ignore);
+            if (step is null) return [];
+            if (steps.Contains(step))
+            {
+                if (!softLoop) throw new InvalidOperationException("Loop detected");
+                return [];
+            }
+            ignore.Add(step);
+            steps.Add(step);
+            next = step;
+        }
+        return steps;
+    }
+    
     public List<List<Post>> GetAlternatives(int first, int last)
     {
         Post start = Posts.First(x => x.Id == first);
         Post end = Posts.First(x => x.Id == last);
 
-        return [[start, end]];
+        var fastest = FastestPath(start, end, softLoop: false).ToList();
+        if (fastest is []) return [];
+        List<List<Post>> routes = [fastest];
+        
+        foreach (var startDirection in start.Directions)
+        {
+            if(startDirection == fastest[1]) continue;
+            List<Post> root = [start];
+            var alternative = FastestPath(startDirection, end, (List<Post>)[start]).ToList();
+            if(alternative.Count == 0) continue;
+            root.AddRange(alternative);
+            routes.Add(root);
+        }
+        
+        foreach (var post in fastest.Select((x, i) => (x, i)).Skip(1).SkipLast(1))
+        {
+            foreach (var direction in post.x.Directions)
+            {
+                if(direction == fastest[post.i+1] || direction == fastest[post.i - 1]) continue;
+                List<Post> root = fastest.Take(post.i).ToList();
+                List<Post> ignored = root.ToList();
+                ignored.Add(direction);
+                var alternative = FastestPath(direction, end, ignored).ToList();
+                if(alternative.Count == 0) continue;
+                root.AddRange(alternative);
+                routes.Add(root);
+            }
+        }
+
+        return routes;
     }
 }
